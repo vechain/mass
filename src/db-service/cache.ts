d1db47847e3b4194c99d579f72228d0e2b160afa
tony <liboliqi@gmail.com>
2021-09-27 18:11:59 +0800

    cache: tx & transfer counts when it's higher than 50k

--- src/db-service/cache.ts
+++ src/db-service/cache.ts
@@ -2,6 +2,10 @@ import * as LRU from 'lru-cache'
 import { REVERSIBLE_WINDOW } from '../utils'
 
 export const cache = new LRU<string, any>(128 * 1024) // 128k cache entries
+export const agedCache = new LRU<string, any>({
+    max: 16 * 2014,
+    maxAge: 6 * 60 * 60 * 1000
+})
 
 export const keys = {
     BEST: 'BEST',
@@ -14,6 +18,8 @@ export const keys = {
     BLOCK_TX: (blockID: string) => { return 'BLOCK_TX' + blockID },
     TX: (txID: string) => { return 'TX' + txID },
     TX_TRANSFER: (txID: string) => { return 'TX_TRANSFER' + txID },
+    TX_COUNT: (addr: string, type: string) => { return `TX_COUNT_${addr}_${type}` },
+    TRANSFER_COUNT: (addr: string, type: string) => { return `TRANSFER_COUNT_${addr}_${type}` }
 }
 
 export const isNonReversible = (blockNum: number) => {
