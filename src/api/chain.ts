0af19be8e15b8dec1bdb2ed5b2efcbd5bcbf6eb5
tony <liboliqi@gmail.com>
2022-11-08 15:26:08 +0800

    add /chain/head and /chain/summary

--- src/api/chain.ts
+++ src/api/chain.ts
@@ -1,11 +1,24 @@
 import { Router } from 'express'
 import { try$ } from 'express-toolbox'
+import { getBest, getRecentBlocks } from '../db-service/block'
 import { getStatus } from '../db-service/chain'
+import { getRecentTransactions } from '../db-service/transaction'
 
 const router = Router()
 export = router
 
-router.get('/status', try$(async (req, res) => {
+router.get('/summary', try$(async (req, res) => {
     const status = await getStatus()
-    res.json(status)
+    const blocks = await getRecentBlocks(10)
+    const txs = await getRecentTransactions(10)
+    res.json({status, blocks, txs})
+}))
+
+router.get('/head', try$(async (req, res) => {
+    const best = await getBest()
+    return res.json({
+        id: best.id,
+        timestamp: best.timestamp,
+        parentID: best.parentID
+    })
 }))
