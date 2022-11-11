35c86233d3f2fcc183938937713da6d6aaf5ce01
tony <liboliqi@gmail.com>
2022-11-11 15:50:36 +0800

    cache chain summary

--- src/api/chain.ts
+++ src/api/chain.ts
@@ -1,17 +1,36 @@
 import { Router } from 'express'
 import { try$ } from 'express-toolbox'
 import { getBest, getRecentBlocks } from '../db-service/block'
-import { getStatus } from '../db-service/chain'
+import { getStatus, ChainStatus } from '../db-service/chain'
 import { getRecentTransactions } from '../db-service/transaction'
+import { Block } from '../explorer-db/entity/block'
+
+interface Summary {
+    status: ChainStatus
+    blocks: Block[]
+    txs: {
+        txID: string;
+        origin: string;
+        totalValue: bigint;
+        reverted: boolean;
+        timestamp: number;
+    }[]
+}
 
 const router = Router()
 export = router
 
+let summary: Summary|null = null
 router.get('/summary', try$(async (req, res) => {
-    const status = await getStatus()
-    const blocks = await getRecentBlocks(10)
-    const txs = await getRecentTransactions(10)
-    res.json({status, blocks, txs})
+    const best = await getBest()
+    if (!summary || summary.status.best !== best.id) {
+        const status = await getStatus(best.id)
+        const blocks = await getRecentBlocks(10)
+        const txs = await getRecentTransactions(10)
+        summary = { status, blocks, txs}
+    }
+   
+    return res.json(summary)
 }))
 
 router.get('/head', try$(async (req, res) => {
